0761a00d3ebb3bfc3715b1d7de183df82a0db967
Wrench0-0 <202380356@psu.palawan.edu.ph>
2026-03-24 20:14:43 +0800

    admins

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Category, Priority, Task, SubTask, Note
 
+class SubTaskInline(admin.TabularInline):
+        model = SubTask
+        extra = 1
+        fields = ("title", "status")
+        show_change_link = True
+class NoteInline(admin.StackedInline):
+        model = Note
+        extra = 1
+        fields = ("content", "created_at")
+        readonly_fields = ("created_at",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'priority', 'category')
+    list_filter = ('status', 'priority', 'category')
+    search_fields = ('title', 'description')
+    inlines = [SubTaskInline, NoteInline]
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'parent_task_name')
+    list_filter = ('status',)
+    search_fields = ('title',)
+
+    @admin.display(description='Parent Task')
+    def parent_task_name(self, obj):
+        return obj.task.title
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('task', 'content', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('content',)
+    
 # Register your models here.
